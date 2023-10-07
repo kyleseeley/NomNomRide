919e3002020b135453fcde673ea4ff34ea22c6b8
Selina Shi <selina@Selinas-MacBook-Air.local>
2023-10-07 15:21:03 -0700

    table menuItem

---
+++ app/models/menuItmes.py
@@ -0,0 +1,21 @@
+from .db import db
+
+
+class MenuItems(db.Model):
+    __tablename__ = 'menuItems'
+
+    id = db.Column(db.Integer, primary_key=True)
+    restaurantId = db.Column(db.Integer,db.foreignKey("restaurant.id"))
+    name = db.Column(db.String(255), nullable=False)
+    type = db.Column(db.String(60), nullable=False)
+    image = db.Column()
+    restaurant = db.relationship("Restaurant",back_populate="id")
+
+def to_dict(self):
+  return {
+    "id": self.id,
+    "restaurantId": self.restaurantId,
+    "name": self.name,
+    "type": self.type,
+    "image":self.image
+  }
