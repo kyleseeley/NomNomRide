155af51e697ec3d8bbd00dab9c43900eab1c3efb
Kyle Seeley <kyleseeley@Kyles-MacBook-Air-2.local>
2023-10-07 15:38:20 -0600

    deleted models branch

--- app/models/restaurant.py
+++ app/models/restaurant.py
@@ -6,3 +6,12 @@ class Restaurant(db.Model):
 
     id = db.Column()
     ownerId = db.Column(db.Integer)
+    address = db.Column(db.String(255), nullable=False)
+    city = db.Column(db.String(255), nullable=False)
+    state = db.Column(db.String(255), nullable=False)
+    country = db.Column(db.String(255), nullable=False)
+    lat = db.Column(db.Float())
+    lng = db.Column(db.Float())
+    name = db.Column(db.String(60), nullable=False)
+    type = db.Column(db.String(60), nullable=False)
+    image = db.Column()
