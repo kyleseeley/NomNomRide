a253eddaa844f49bcfd1f675f7bfc7f5875ed8c8
joshh5790 <joshh5790@gmail.com>
2023-10-11 16:02:57 -0700

    remove country, assuming users are located in US

--- app/forms/restaurant_form.py
+++ app/forms/restaurant_form.py
@@ -16,14 +16,6 @@ def validateState(form, field):
   state = field.data
   if not state:
     raise ValidationError('Please enter an state.')
-  if len(state) > 50:
-    raise ValidationError('State name is too long.')
-def validateCountry(form, field):
-  country = field.data
-  if not country:
-    raise ValidationError('Please enter an country.')
-  if len(country) > 50:
-    raise ValidationError('Country name is too long.')
 def validateLat(form, field):
   lat = field.data
   if not lat:
@@ -56,8 +48,7 @@ def validateImage(form, field):
 class RestaurantForm(FlaskForm):
   address = StringField('address', validators=[DataRequired(), Length(max=50), validateAddress])
   city = StringField('city', validators=[DataRequired(), Length(max=50), validateCity])
-  state = StringField('state', validators=[DataRequired(), Length(max=50), validateState])
-  country = StringField('country', validators=[DataRequired(), Length(max=50), validateCountry])
+  state = StringField('state', validators=[DataRequired(), validateState])
   lat = FloatField('lat', validators=[DataRequired(), NumberRange(min=-90, max=90), validateLat])
   lng = FloatField('lng', validators=[DataRequired(), NumberRange(min=-180, max=180), validateLng])
   name = StringField('name', validators=[DataRequired(), Length(max=40), validateName])
