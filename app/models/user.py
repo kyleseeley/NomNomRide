52ef5b14c37ebb1acd5213c0f9f49d1292a4a8c2
Kyle Seeley <kyleseeley@Kyles-MacBook-Air-2.local>
2023-10-21 15:45:21 -0600

    added cascades to user model

--- app/models/user.py
+++ app/models/user.py
@@ -23,11 +23,11 @@ class User(db.Model, UserMixin):
     lng = db.Column(db.Float())
     hashed_password = db.Column(db.String(255), nullable=False)
 
-    restaurants = db.relationship("Restaurant", back_populates="owner")
+    restaurants = db.relationship("Restaurant", back_populates="owner", cascade="all, delete-orphan")
 
-    reviews = db.relationship("Review", back_populates="user")
+    reviews = db.relationship("Review", back_populates="user", cascade="all, delete-orphan")
 
-    shoppingCart = db.relationship("ShoppingCart", back_populates="user")
+    shoppingCart = db.relationship("ShoppingCart", back_populates="user", cascade="all, delete-orphan")
 
     @property
     def password(self):
